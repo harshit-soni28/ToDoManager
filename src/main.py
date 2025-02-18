c21bc73a5b1c4dd9f2b219f48b88befe058f31cf
harshit-soni28 <harshit.soni@onlinesales.ai>
2025-02-18 19:06:44 +0530

    refactoring project tree

---
+++ src/main.py
@@ -0,0 +1,35 @@
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+
+from utils.addNewTask import addNewTask
+from utils.markComplete import markComplete
+from utils.deleteTask import deleteTask
+from utils.listAllTasks import listAllTasks
+import utils.state as state
+
+while True:
+    print("\n1. Add new task")
+    print("2. Mark task as complete")
+    print("3. Delete task")
+    print("4. List all tasks")
+    print("5. Exit")
+    choice = int(input("Enter your choice: "))
+    if choice == 1:
+        description = input("Enter task description: ")
+        dueDate = input("Enter due date: ")
+        print(addNewTask(state.tasks, description, dueDate))
+    elif choice == 2:
+        task_id = int(input("Enter task number to mark as complete: "))
+        tasks = markComplete(state.tasks, task_id)
+    elif choice == 3:
+        task_id = int(input("Enter task number to delete: "))
+        tasks = deleteTask(state.tasks, task_id)
+    elif choice == 4:
+        listAllTasks(state.tasks)
+    elif choice == 5:
+        break
+    else:
+        print("Invalid choice")
